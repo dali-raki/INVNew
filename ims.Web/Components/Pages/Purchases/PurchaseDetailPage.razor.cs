f78a7d8e1c12b2883b78c3df46829c26167b27d9
RACHEDI radouane <148220975+radoin082-hub@users.noreply.github.com>
2025-03-16 16:33:15 +0100

    Delete ims.Web directory

--- ims.Web/Components/Pages/Purchases/PurchaseDetailPage.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace ims.Web.Components.Pages.Purchases
-{
-    public partial class PurchaseDetailPage
-    {
-        [Parameter] public Guid Id { get; set; }
-    }
-}
